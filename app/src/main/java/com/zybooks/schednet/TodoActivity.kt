7e118fc7b9dcd59a1f18fbe74c4e9705a191f700
Vague-INKD-2 <matthewtiggers99@gmail.com>
2022-10-19 08:32:45 -0400

    Added MongoDB Realms to gradle file

--- app/src/main/java/com/zybooks/schednet/TodoActivity.kt
+++ app/src/main/java/com/zybooks/schednet/TodoActivity.kt
@@ -1,10 +1,9 @@
 package com.zybooks.schednet
 
-//import androidx.appcompat.app.AppCompatActivity
 import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : Activity() {
+class TodoActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/zybooks/schednet/TodoRibbonObj.kt
@@ -0,0 +1,64 @@
+package com.zybooks.schednet
+
+class TodoRibbonObj {
+    var Tdo_Name = "todo_nam"
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_Description = "empty"
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_isAllDay = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_DateA = "datestr:timestr"
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_DateB = "datestr:timestr"
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_isRepeating = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_isRemind = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_isRepeatType = "type"
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var Tdo_numRemind = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    //Struct ==> "datestr:timestr"
+    var Tdo_listRepeatRemindTime: List<String> = listOf("datestr:timestr", "startReminds") //Index 0 = repeat elem, Index 1+ = remind elem
+        get() = field
+        set(value) {
+            field = value
+        }
+}
